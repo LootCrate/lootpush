7d696a6694358505084fde7ff36ad7e7054c43a0
Dave Yu <dave.yu@lootcrate.com>
2019-01-31 15:52:38 -0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+ruby '2.6.0'
+
+gem 'firebase_cloud_messenger'
+gem 'http'
+gem 'pg'
+gem 'pry'
+gem 'pry-byebug'
+gem 'semantic_logger'
+gem 'sequel'
---
+++ push.rb
@@ -0,0 +1,72 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'http'
+require 'yaml'
+require 'semantic_logger'
+require 'firebase_cloud_messenger'
+require 'sequel'
+require 'pg'
+require 'pry'
+
+require './env.rb'
+
+logger = SemanticLogger['LootPush']
+SemanticLogger.add_appender(io: $stdout, formatter: :color, level: :debug)
+DB = Sequel.connect(LOOTCRATE_DATABASE_URL, logger: logger)
+FirebaseCloudMessenger.project_id = FIREBASE_PROJECT_ID
+
+###################################################
+## 1. Parse config yaml                          ##
+###################################################
+config_file = ARGV[0]
+logger.info("Loading config from #{config_file}")
+message_config = YAML.load_file(config_file)['message']
+
+message_topic = "#{message_config['topic']}"
+message_title = message_config['title']
+message_body = message_config['body']
+message_recipients = message_config['recipients']
+
+logger.info("Push Notification Topic: #{message_topic}")
+logger.info("Push Notification Title: #{message_title}")
+logger.info("Push Notification Message: #{message_body}")
+logger.info("Email address count: #{message_recipients.count}")
+
+###########################################################
+##  2. Lookup firebase registration keys for             ##
+##  each email address and subscribe them to the topic   ##
+###########################################################
+devices = DB[:devices].join(:users, id: :user_id).where(email: message_recipients, status: 0).select(:firebase_registration_token)
+tokens = devices.map(:firebase_registration_token).uniq
+logger.info("Found matching #{tokens.count} registered devices with notifications enabled")
+
+## Batch add the devices to the topic
+logger.info("Subscribing devices to topic #{message_topic} in batches of 1,000")
+google_api_headers = {
+  "Content-Type": "application/json",
+  "Authorization": "key=#{FIREBASE_CLOUD_MESSAGING_SERVER_KEY}"
+}
+tokens.each_slice(1000) do |tokens_slice| 
+  body = {
+    to: "/topics/#{message_topic}",
+    registration_tokens: tokens_slice
+  }
+  HTTP.use(logging: {logger: logger}).headers(google_api_headers).post('https://iid.googleapis.com/iid/v1:batchAdd', json: body)
+end
+
+###################################################
+## 3. Create FCM message and send the push       ##
+## notifcation to the topic                      ##
+###################################################
+notification = FirebaseCloudMessenger::Notification.new(title: message_title, body: message_body)
+message = FirebaseCloudMessenger::Message.new(topic: message_topic, notification: notification)
+if message.valid?(against_api: true)
+  begin
+    FirebaseCloudMessenger.send(message: message)
+  rescue FirebaseCloudMessenger::Error => e
+    logger.error(e)
+  end
+else
+  logger.warn("Invalid message. #{message.errors}")
+end
